2654e6870cc54c07fd24f3d47f295d2f6ecb1a65
Kim Duclos <kimduclos@gmail.com>
2019-01-29 08:44:00 -0700

    clock counting

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,23 @@
+var MSTSeconds = 0;
+setInterval(function() {
+document.getElementById('msTens').innerHTML = MSTSeconds++;
+}, 3000);
+
+// var MSHSeconds = 0;
+// setInterval(function() {
+// document.getElementById('msHundreds').innerHTML = MSHSeconds++;
+// }, 3000);
+
+// var onesSeconds = 0;
+// setInterval(function() {
+// document.getElementById('secondOnes').innerHTML = onesSeconds++;
+// }, 3000);
+
+// var tensSeconds = 0;
+// setInterval(function() {
+// document.getElementById('secondTens').innerHTML = tensSeconds++;
+// }, 3000);
+
+
+
+
